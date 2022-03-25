2c42770bcc429a0f4fa63fa0555a7842931240c3
kaza <kazamidoripan@qq.com>
2022-03-25 17:06:45 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.dao.PlaceDao
 import com.sunnyweather.android.logic.model.Place
 
 class PlaceViewModel : ViewModel() {
@@ -21,4 +22,10 @@ class PlaceViewModel : ViewModel() {
     }
 
 
+
+    fun savePlace(place: Place)= Repository.savePlace(place)
+
+    fun getSavedPlace()= Repository.getSavedPlace()
+
+    fun isPlaceSaved()= Repository.isPlaceSaved()
 }
